c84540a4b90e123b57dfd7c647431a88d420c672
cialamo93 <ignacioalamo93@gmail.com>
2023-12-19 22:37:12 -0300

    primer commit

---
+++ public/assets/js/mercadoPago.js
@@ -0,0 +1,56 @@
+        const mp = new MercadoPago('TEST-085e5a8c-5bde-4e9f-b786-09fb68ee66dd', {
+          locale: 'es-CL'
+        });
+        const bricksBuilder = mp.bricks();
+        const renderCardPaymentBrick = async (bricksBuilder) => {
+          const settings = {
+            initialization: {
+              amount: 100, // monto a ser pago
+              payer: {
+                email: "",
+              },
+            },
+            customization: {
+              visual: {
+                style: {
+                  customVariables: {
+                    theme: 'default', // | 'dark' | 'bootstrap' | 'flat'
+                  }
+                }
+              },
+                paymentMethods: {
+                  maxInstallments: 1,
+                }
+            },
+            callbacks: {
+              onReady: () => {
+                // callback llamado cuando Brick esté listo
+              },
+              onSubmit: async (cardFormData) => {
+                
+                let paymentPending = await fetch("/process_payment", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(cardFormData)
+                });
+                let serverResponse = await paymentPending.json();
+                if (serverResponse === 'approved') {
+                  document.getElementById("cardPaymentBrick_container").style.display = "none";
+                  document.getElementById("successPaymentDiv").style.display = "block";
+                }
+              },
+              onError: (error) => {
+                document.getElementById("cardPaymentBrick_container").style.display = "none";
+                document.getElementById("errorPaymentDiv").style.display = "block";
+            },
+            },
+          };
+          window.cardPaymentBrickController = await bricksBuilder.create('cardPayment', 'cardPaymentBrick_container', settings);
+        };
+
+        setTimeout(() => {
+            renderCardPaymentBrick(bricksBuilder);
+        }, 1500);
+        
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require("cors");
+const { MercadoPagoConfig, Payment } = require('mercadopago');
+
+const port = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"))
+
+const server = http.createServer(app);
+
+const meliClient = new MercadoPagoConfig({ accessToken: 'TEST-7607088986538417-121213-34786fe6cff5243c91cc3f898842a42a-138387612', options: { timeout: 5000 } });
+const meliPayment = new Payment(meliClient);
+
+
+function genTimeID(length) {
+  let letters = "abcdefghijklmnopqrstuvwxyz"
+  let result = letters.charAt(Math.floor(Math.random() * letters.length))
+  let characters =
+      "AaBb1CcDdEe2FfGg3HhIi4JjKk5LlMm6NnOoPp7QqRrSs8TtUu9VvWwXx0YyZz"
+  let charactersLength = characters.length
+  for (let i = 0; i < length; i++) { result += characters.charAt( Math.floor(Math.random() * charactersLength) ) };
+  let timeStamp = Date.now().toString()
+  result += timeStamp.substr(timeStamp.length - 4)
+  return result
+}
+
+
+(async () => {
+
+console.log("Servidor Iniciado a las " + new Date())
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
+
+app.get('/testNode', (req, res) => {
+  console.log("Hola " + new Date());
+});
+
+app.post('/process_payment', async (req, res) => {
+  try {
+    let paymentInfo = await meliPayment.create({
+      body: req.body
+    });
+    res.json(paymentInfo.status)
+  }
+  catch (error) {
+    console.log(error);
+    res.json("error");
+  }
+});
+
+})()
+
