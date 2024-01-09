99fed227feab0688420eb00369131cc45e9db174
cialamo93 <ignacioalamo93@gmail.com>
2024-01-09 00:36:12 -0300

    video listo

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -88,4 +88,18 @@
         document.querySelector(".fixed-box").style.display = "none";
       });
       
+      function changeContentOnResize() {
+        var myDiv = document.getElementById('firstCarousel');
+        var screenWidth = window.innerWidth;
+
+        if (screenWidth <= 768) {
+            myDiv.innerHTML = '<div class="video-container"> <video class="fullscreen-video" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el tag de video. </video> <div class="overlay-text"> <h1 style="color: white" >CAP Oriente</h1> <p style="color: white" >Tu socio estratégico en el mundo de las importaciones...</p> <a href="#aboutUs" class="main-btn rounded-one">Saber Más</a> </div> </div>';
+        }
+        else {
+          myDiv.innerHTML = '<div class="container"> <div class="row"> <div class="col-lg-6"> <div class="slider-content"> <h1 class="title">CAP Oriente</h1> <p class="text">Tu socio estratégico en el mundo de las importaciones. Nos destacamos por simplificar el proceso de importación y ofrecer soluciones innovadoras respaldadas por una experiencia de más de 10 años en el rubro.</p> <ul class="slider-btn rounded-buttons"> <li><a class="main-btn rounded-one" href="#aboutUs">Saber Más</a></li> </ul> </div> </div> </div> </div> <div class="slider-image-box d-none d-lg-flex align-items-center"> <div class="slider-image"> <div> <video width="300" height="600" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el video. </video> </div> </div> </div>';
+      }
+    }
+    window.onload = changeContentOnResize;
+    window.onresize = changeContentOnResize;
+
         
