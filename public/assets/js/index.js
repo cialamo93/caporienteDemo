c48adf24978ab0e9c55a58f90ed9be147b9cc1fd
cialamo93 <ignacioalamo93@gmail.com>
2024-01-16 15:16:34 -0300

    reel chico puesto de nuevo

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -88,22 +88,22 @@
         document.querySelector(".fixed-box").style.display = "none";
       });
       
-    //   function changeContentOnResize() {
-    //     let firstCarousel = document.getElementById('firstCarousel');
-    //     let secondCarousel = document.getElementById('secondCarousel');
-    //     let screenWidth = window.innerWidth;
+      function changeContentOnResize() {
+        let firstCarousel = document.getElementById('firstCarousel');
+        let secondCarousel = document.getElementById('secondCarousel');
+        let screenWidth = window.innerWidth;
 
-    //     if (screenWidth <= 768) {
-    //         firstCarousel.innerHTML = '<div class="video-container"> <video class="fullscreen-video" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el tag de video. </video> <div class="overlay-text"> <h1 style="color: white" >CAP Oriente</h1> <p style="color: white" >Tu socio estratégico en el mundo de las importaciones...</p> <a href="#aboutUs" class="main-btn rounded-one">Saber Más</a> </div> </div>';
-    //         secondCarousel.innerHTML = '<div class="video-container"> <video class="fullscreen-video" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el tag de video. </video> <div class="overlay-text"> <h3 style="color: white" >Próximo viaje de capacitación y negocios Abril 2024</h3> <p style="color: white" >Dubai, Shanghai, Yiwu, Cantón, Tailandia.</p> <a href="#nextTrip" class="main-btn rounded-one">Saber Más</a> </div> </div>';
-    //       }
-    //     else {
-    //       firstCarousel.innerHTML = '<div class="container"> <div class="row"> <div class="col-lg-6"> <div class="slider-content"> <h1 class="title">CAP Oriente</h1> <p class="text">Tu socio estratégico en el mundo de las importaciones. Nos destacamos por simplificar el proceso de importación y ofrecer soluciones innovadoras respaldadas por una experiencia de más de 10 años en el rubro.</p> <ul class="slider-btn rounded-buttons"> <li><a class="main-btn rounded-one" href="#aboutUs">Saber Más</a></li> </ul> </div> </div> </div> </div> <div class="slider-image-box d-none d-lg-flex align-items-center"> <div class="slider-image"> <div> <video width="300" height="600" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el video. </video> </div> </div> </div>';
-    //       secondCarousel.innerHTML = '<div class="container"> <div class="row"> <div class="col-lg-6"> <div class="slider-content"> <h1 class="title">Próximo viaje de capacitación y negocios Abril 2024</h1> <p class="text">Dubai, Shanghai, Yiwu, Cantón, Tailandia.</p> <ul class="slider-btn rounded-buttons"> <li><a class="main-btn rounded-one" href="#nextTrip">Saber Más</a></li> </ul> </div> </div> </div> </div> <div class="slider-image-box d-none d-lg-flex align-items-center"> <div class="slider-image" style="margin-left: 5rem;"> <div> <video width="300" height="600" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el video. </video> </div> </div> </div>';
-    //   }
-    // }
-    // window.onload = changeContentOnResize;
-    // window.onresize = changeContentOnResize;
+        if (screenWidth <= 768) {
+            firstCarousel.innerHTML = '<div class="video-container"> <video class="fullscreen-video" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el tag de video. </video> <div class="overlay-text"> <h1 style="color: white" >CAP Oriente</h1> <p style="color: white" >Tu socio estratégico en el mundo de las importaciones...</p> <a href="#aboutUs" class="main-btn rounded-one">Saber Más</a> </div> </div>';
+            secondCarousel.innerHTML = '<div class="video-container"> <video class="fullscreen-video" autoplay muted loop playsinline> <source src="assets/images/promoVid.mp4" type="video/mp4"> Tu navegador no soporta el tag de video. </video> <div class="overlay-text"> <h3 style="color: white" >Próximo viaje en Abril 2024</h3> <p style="color: white" >Dubai, Shanghai, Yiwu, Cantón, Tailandia.</p> <a href="#nextTrip" class="main-btn rounded-one">Saber Más</a> </div> </div>';
+          }
+        else {
+          firstCarousel.innerHTML = '<div class="container"> <div class="row"> <div class="col-lg-6"> <div class="slider-content"> <h1 class="title">CAP Oriente</h1> <p class="text">Tu socio estratégico en el mundo de las importaciones. Nos destacamos por simplificar el proceso de importación y ofrecer soluciones innovadoras respaldadas por una experiencia de más de 10 años en el rubro.</p> <ul class="slider-btn rounded-buttons"> <li><a class="main-btn rounded-one" href="#aboutUs">Saber Más</a></li> </ul> </div> </div> </div> </div>';
+          secondCarousel.innerHTML = '<div class="container"> <div class="row"> <div class="col-lg-6"> <div class="slider-content"> <h1 class="title">Próximo viaje en Abril 2024</h1> <p class="text">Dubai, Shanghai, Yiwu, Cantón, Tailandia.</p> <ul class="slider-btn rounded-buttons"> <li><a class="main-btn rounded-one" href="#nextTrip">Saber Más</a></li> </ul> </div> </div> </div> </div>';
+      }
+    }
+    window.onload = changeContentOnResize;
+    window.onresize = changeContentOnResize;
 
     function downloadPDF() {
       const rutaImagen = '/assets/images/files/brochure2.pdf';
@@ -125,8 +125,6 @@
   let serverRequest = await fetch('https://mindicador.cl/api');
   let serverResponse = await serverRequest.json(); console.log(serverResponse);
   let codes = ['dolar', 'euro', 'imacec', 'ipc', 'uf', 'utm'];
-  // const arrayFiltrado = arrayDeObjetos.filter(a => codes.includes(a.codigo));
-
   let data = Object.values(serverResponse).filter(a => codes.includes(a.codigo)).map(a => {
     return {Nombre: a.codigo, Valor: a.valor}
   });
